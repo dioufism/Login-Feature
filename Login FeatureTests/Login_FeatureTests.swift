c7e33db99b05545d2af11001fc1e43e2c041a4d9
Ousmane <dioufism@gmail.com>
2023-02-22 12:55:13 -0600

    setting up unit test

--- Login FeatureTests/Login_FeatureTests.swift
+++ Login FeatureTests/Login_FeatureTests.swift
@@ -6,25 +6,26 @@
 //
 
 import XCTest
+import Alchemy
+
 @testable import Login_Feature
 
 final class Login_FeatureTests: XCTestCase {
 
+    var handler: AuthLayer?
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        self.handler = AuthLayer()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        try super.tearDownWithError()
+        self.handler =  nil    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testAuthenticationPassed() { }
+    
+    func testAuthenticationFailed() { }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
